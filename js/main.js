4398bca013cf084362ddf06b063ae59afbaa819a
Jamaltscode <jamalts@code.edu.az>
2022-07-10 17:05:49 +0400

    To do list

---
+++ js/main.js
@@ -0,0 +1,17 @@
+
+
+
+/*var icon = document.querySelector('#eye')
+
+icon.addEventListener('click', function(){
+    var input = document.querySelector('#input')
+    var type = input.getAttribute('type')
+    if(type==='password'){
+        input.setAttribute('type', 'text')
+        icon.className ='fa-solid fa-face-rolling-eyes'
+    }
+    else{
+        input.setAttribute('type', 'password')
+        icon.className ='fa-solid fa-eye'
+    }
+}) */
